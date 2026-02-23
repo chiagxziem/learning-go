3c3292a73ad7872806a0d18ece28b7fc82d06b9d
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2026-02-23 23:24:24 +0100

    finish ch 04

--- ch_04/blocks.go
+++ ch_04/blocks.go
@@ -9,6 +9,10 @@ func main() {
 	// Blocks, Shadows, and Control Structures
 
 	blocks()
+	ifElseStatements()
+	forStatements()
+	switchStatements()
+	exercises()
 }
 
 func blocks() {
@@ -35,13 +39,16 @@ func blocks() {
 
 	x2 := 10
 	if x2 > 5 {
-		x2, y2 := 5, 20     // This second x2 is shadowing the first x2, instead of being a reassignment.
+		x2, y2 := 5, 20 // This second x2 is shadowing the first x2, instead of being a reassignment.
+		// In clearer terms, the assignment to x2 inside the block only applies inside the block.
 		fmt.Println(x2, y2) // 5, 20
 	}
 	fmt.Println(x2) // 10
 
 	// Imported packages can also be shadowed
+}
 
+func ifElseStatements() {
 	//* if and else
 	// the biggest diff between Go and other langs in if else statements is that Go doesnt have
 	// a parentheses around the condition.
@@ -67,7 +74,9 @@ func blocks() {
 	}
 
 	// In the example above, note that we shadowed the first ranN.
+}
 
+func forStatements() {
 	//* for
 	// In Go, `for` is the only looping keyword there is. it can be used in four formats
 
@@ -79,14 +88,14 @@ func blocks() {
 		fmt.Println(i)
 	}
 
-	// the initialization can be left out if the `for` loop is based on an already declared value.
+	// the initialization can be left out if the `for` loop is based on an already declared value. the two semicolons are preserved.
 
 	i1 := 0
 	for ; i1 > 10; i1++ {
 		fmt.Println(i1)
 	}
 
-	// or the increment i left out because you have a more complicated increment rule inside the loop
+	// or the increment is left out because you have a more complicated increment rule inside the loop. the two semicolons are also preserved.
 
 	for i := 0; i > 10; {
 		fmt.Println(i)
@@ -110,7 +119,7 @@ func blocks() {
 
 	// 3. Infinite `for` Statement
 	// here, the initialization, condition and increment are all done away with. this creates a loop
-	// that will run infintely until a break keyword is used. thi is Go's equivalent of the classic
+	// that will run infintely until a break keyword is used. this is Go's equivalent of the classic
 	// `do while` loop.
 
 	i3 := 0
@@ -211,7 +220,94 @@ outer:
 		}
 		fmt.Println()
 	}
+}
 
-  //* switch
-  
+func switchStatements() {
+	//* switch
+	// just like in the if else statements, no parenthesis are put around the value being compared.
+	// also like the if else statements, a variable can be declared in the switch statement and itll be scoped to all the cases of the switch statement.
+
+	// all case clauses are contained inside a bracket. the default clause is optional. each case is a block of its own and can have multiple lines of code. no brackets are used to contain cases.
+
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "is exactly the rigth length", wordLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word!")
+		}
+	}
+
+	// if a case is empty, then nothing happens. thats why in the sample above, the words "gopher", and "octopus" are not printed to the console.
+
+	// the switch statement does have a `fallthrough` keyword but it should be used at all. restructure your code if you find yourself needing to use it.
+
+	// the `break` keyword can also be used inside a case of a switch, but should be used sparingly. you should only use a `break` in a case of a switch, if you want to break out of a containing `for` loop. in that scenario, the for loop must be labelled, else Go will asssume you want to just break out of the case and not the loop.
+
+loop:
+	for i := range 10 {
+		switch i {
+		case 0, 2, 4, 6:
+			fmt.Println(i, "is even!")
+		case 3:
+			fmt.Println(i, "is divisble by 3 but not 2")
+		case 7:
+			fmt.Println("exit the loop!")
+			break loop
+		default:
+			fmt.Println(i, "is boring")
+		}
+	}
+
+	// Blank Switches
+	// switches can also be written in such a way that the value being compared against isnt specified. its called a blank switch. a regular switch only allows you ti check a value for equality. a blank switch allows you to use any boolean comarison for each case.
+
+	names := []string{"Emeka", "Uche", "Chinero", "Ada", "Chiagoziem", "Kamharida"}
+
+	for _, name := range names {
+		switch nameLen := len(name); {
+		case nameLen < 5:
+			fmt.Println(name, "is a short name!")
+		case nameLen > 7:
+			fmt.Println(name, "is a long name!")
+		default:
+			fmt.Println(name, "is exactly the right length.")
+		}
+	}
+}
+
+func exercises() {
+	// exercise 1
+	intSl := []int{}
+	for len(intSl) <= 100 {
+		randNo := rand.Intn(100)
+		intSl = append(intSl, randNo)
+	}
+	fmt.Println("Int Slice:", intSl)
+
+	// exercise 2
+	for _, v := range intSl {
+		if v%2 == 0 && v%3 == 0 {
+			fmt.Println(v, "Six!")
+			continue
+		}
+
+		if v%2 == 0 {
+			fmt.Println(v, "Two!")
+			continue
+		}
+
+		if v%3 == 0 {
+			fmt.Println(v, "Three!")
+			continue
+		}
+
+		fmt.Println(v, "Never mind!")
+	}
 }
